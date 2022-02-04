6fbec18d9d3bfc602fd88e83c3e57dc06329ea77
ansarav <ansaravi@ucsd.edu>
2022-02-04 15:39:07 -0800

    uu change made to make pass

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,7 +4,7 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void Subtract() {
-        assertEquals(3, SkillDemo.Subtract(5,1));// purposefully make it fail
+        assertEquals(3, SkillDemo.Subtract(4,1));// purposefully make it fail
         // that way you can pull and in github 
         
     }
